b3fff581f42619c56b8c649ca8da8f7a9551b9e4
lavsexpert <lavrov-sergey@yandex.ru>
2020-02-05 13:35:53 +0300

    Сервер соединен с клиентом
    - Теперь все перемещения по страницам обрабатываются на сервере
    - Если запрашивается страница, для которой нет соответствующего файла на сервере, то выводится страница 404.html
    - Если в адресе есть параметры после ?, то они пока обрезаются
    - Установлена кодировка везде UTF-8
    - В .gitignore добавлена строчка, чтобы кэш не хранился в git

--- server/requests.py
+++ server/requests.py
@@ -3,9 +3,37 @@ from http.server import BaseHTTPRequestHandler
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        message = "Сервер онлайн..."
-        self.wfile.write(bytes(message, "cp1251"))
-        return message
+
+        path = self.path
+        if path == "/":
+            path = "/index.html"
+
+        if path == "/favicon.ico":
+            f = open("../client/img/favicon.ico", 'rb')
+            self.send_response(200)
+            self.send_header('Content-type', 'image/ico')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        elif path.split("/")[1] == "img":
+            f = open("../client" + path, 'rb')
+            self.send_response(200)
+            self.send_header('Content-type', 'image/*,image/jpeg,image/png')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        else:
+            try:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                path = path.split("?")[0]
+                file = open("../client" + path, "r", encoding='utf-8')
+                message = file.read()
+            except FileNotFoundError:
+                file = open("../client/errors/404.html", "r", encoding='utf-8')
+                message = file.read()
+            file.close()
+
+            self.wfile.write(bytes(message, "utf-8"))
+        return
