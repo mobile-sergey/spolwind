6c3f6556e7dde89dc4a3158992f86078e479bdfd
lavsexpert <lavrov-sergey@yandex.ru>
2020-02-05 14:05:50 +0300

    Рефакторинг сервера
    - Упрощён код за счёт вынесения повторяющегося кода в функции

--- server/requests.py
+++ server/requests.py
@@ -9,31 +9,32 @@ class RequestHandler(BaseHTTPRequestHandler):
             path = "/index.html"
 
         if path == "/favicon.ico":
-            f = open("../client/img/favicon.ico", 'rb')
-            self.send_response(200)
-            self.send_header('Content-type', 'image/ico')
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
+            self.set_headers('image/ico')
+            file = open("../client/img/favicon.ico", 'rb')
+            self.get_content(file, file.read())
         elif path.split("/")[1] == "img":
-            f = open("../client" + path, 'rb')
-            self.send_response(200)
-            self.send_header('Content-type', 'image/*,image/jpeg,image/png')
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
+            self.set_headers('image/*,image/jpeg,image/png')
+            file = open("../client" + path, 'rb')
+            self.get_content(file, file.read())
         else:
+            self.set_headers('text/html')
             try:
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                path = path.split("?")[0]
-                file = open("../client" + path, "r", encoding='utf-8')
-                message = file.read()
+                file = open("../client" + path.split("?")[0], "r", encoding='utf-8')
             except FileNotFoundError:
                 file = open("../client/errors/404.html", "r", encoding='utf-8')
-                message = file.read()
-            file.close()
-
-            self.wfile.write(bytes(message, "utf-8"))
+            self.get_content(file, bytes(file.read(), "utf-8"))
         return
+
+    def set_headers(self, content_type):
+        self.send_response(200)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+        return
+
+    def get_content(self, file, content):
+        self.wfile.write(content)
+        file.close()
+
+
+
+
